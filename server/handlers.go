91632dc27e8a09c15dd7e7e8d017329cbcf14123
Prithvish Baidya <deformercoding@gmail.com>
2024-04-07 07:49:05 +0530

    Allow Credentials

--- server/handlers.go
+++ server/handlers.go
@@ -9,6 +9,7 @@ import (
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Crendentials", "true")
 }
 
 func ServeProgressiveEpisode(p *podcast.Podcast) http.HandlerFunc {
